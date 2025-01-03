13d66f6940bf2ea5d9ebf200787e9dc25c6ab5c5
Brother-Eaves <david.g.t.eaves@gmail.com>
2025-01-02 22:04:43 -0500

    added removeFrom Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const output = []
+   array.forEach((index) => {
+    if (!args.includes(index)) {
+        output.push(index)
+    } 
+   });
+    return output
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray; 
