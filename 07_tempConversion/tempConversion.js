0f4ce94f71c040825f87a749a1a5807f99a5b989
Brother-Eaves <david.g.t.eaves@gmail.com>
2025-01-13 22:20:38 -0500

    Add functionality to module

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(foo) {
+  let C = (foo - 32) * .5555555556
+  let cRound = Math.round(C * 10) / 10
+  return cRound
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(barr) {
+  let F = (barr * 1.8) + 32
+  let fRound = Math.round(F * 10) /10
+  return fRound
 };
 
 // Do not edit below this line
